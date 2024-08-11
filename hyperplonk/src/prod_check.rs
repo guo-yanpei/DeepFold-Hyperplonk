cbcb3a2c4f6917d8aaf55ba09448f0d160e471f4
Yanpei Guo <2847399255@qq.com>
2024-08-11 12:21:39 +0800

    1

--- hyperplonk/src/prod_check.rs
+++ hyperplonk/src/prod_check.rs
@@ -33,9 +33,10 @@ impl ProdCheck {
                     evals_1.push(products[i][j]);
                 }
             }
-            let (mut new_point, v) = Sumcheck::prove([evals_0, evals_1, eq.evals], 3, transcript, |v: [F; 3]| {
-                v[0] * v[1] * v[2]
-            });
+            let (mut new_point, v) =
+                Sumcheck::prove([evals_0, evals_1, eq.evals], 3, transcript, |v: [F; 3]| {
+                    v[0] * v[1] * v[2]
+                });
             transcript.append_f(v[0]);
             transcript.append_f(v[1]);
             let r = transcript.challenge_f();
@@ -45,5 +46,54 @@ impl ProdCheck {
         point
     }
 
-    fn verify<F: Field>(mut y: F, var_num: usize, transcript: &mut Transcript, proof: &mut Proof) {}
+    fn verify<F: Field>(mut y: F, var_num: usize, transcript: &mut Transcript, proof: &mut Proof) -> (Vec<F>, F) {
+        let mut v0: F = proof.get_next_and_step();
+        let mut v1: F = proof.get_next_and_step();
+        assert_eq!(v0 * v1, y);
+        transcript.append_f(v0);
+        transcript.append_f(v1);
+        let mut point = vec![transcript.challenge_f::<F>()];
+        for i in 1..var_num {
+            y = v0 + (v1 - v0) * point[0];
+            let (mut new_point, y) = Sumcheck::verify(y, 3, i, transcript, proof);
+            v0 = proof.get_next_and_step();
+            v1 = proof.get_next_and_step();
+            assert_eq!(v0 * v1 * MultiLinearPoly::eval_eq(&new_point, &point), y);
+            transcript.append_f(v0);
+            transcript.append_f(v1);
+            let r = transcript.challenge_f();
+            point = vec![r];
+            point.append(&mut new_point);
+        }
+        y = v0 + (v1 - v0) * point[0];
+        (point, y)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use arithmetic::{field::{goldilocks64::Goldilocks64Ext, Field}, poly::MultiLinearPoly};
+    use rand::thread_rng;
+    use util::fiat_shamir::Transcript;
+
+    use super::ProdCheck;
+
+    #[test]
+    fn prod_check() {
+        let mut transcript = Transcript::new();
+        let mut rng = thread_rng();
+        let evals = (0..4096)
+            .map(|_| Goldilocks64Ext::random(&mut rng))
+            .collect::<Vec<_>>();
+        let prod = evals
+            .iter()
+            .fold(Goldilocks64Ext::one(), |acc, x| acc * x.clone());
+        let point = ProdCheck::prove(evals.clone(), &mut transcript);
+        let mut proof = transcript.proof;
+
+        let mut transcript = Transcript::new();
+        let (new_point, y) = ProdCheck::verify(prod, 12, &mut transcript, &mut proof);
+        assert_eq!(point, new_point);
+        assert_eq!(MultiLinearPoly::eval_multilinear_ext(&evals, &point), y);
+    }
 }
